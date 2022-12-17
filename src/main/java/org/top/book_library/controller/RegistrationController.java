e4a585d234f0090cc3a640a80f050fc5a105871d
Pominov7 <Pominov7@mail.ru>
2022-12-17 11:16:10 +0700

    added validation in registration

--- src/main/java/org/top/book_library/controller/RegistrationController.java
+++ src/main/java/org/top/book_library/controller/RegistrationController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.top.book_library.db.entity.security.User;
@@ -19,25 +21,28 @@ public class RegistrationController {
 
     // Обработчик для получения формы регистрации пользователя
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(User user, Model model) {
+        model.addAttribute("user", user);
         return "layout/registration";
     }
 
     // Обработчик для добавления зарегистрированного пользователя
     @PostMapping("/registration")
-    public String addUser(@Valid User user, Model model, BindingResult result,
+    public String addUser(@Valid User user, BindingResult result,
                           RedirectAttributes redirectAttributes) {
-        if (result.hasErrors()) {
-            return "/index";
-        }
         try {
-        User userFromDb = userService.findByUsername(user.getUsername());
+            if (userService.findByUsername(user.getUsername()) != null) {
+                result.addError(new FieldError("user", "username", "User "
+                        + user.getUsername() + " exists!"));
+            }
+            if (userService.findByUsername(user.getPassword()) != null) {
+                result.addError(new FieldError("user", "password", "Password "
+                        + user.getUsername() + " exists!"));
+            }
 
-        if (userFromDb != null) {
-            model.addAttribute("message", "User " + user.getUsername() + " exists!");
-            return "layout/registration";
-        }
-
+            if (result.hasErrors()) {
+                return "layout/registration";
+            }
             userService.addUser(user);
             redirectAttributes.addFlashAttribute("message",
                     "Registration was successful!");
--- src/main/java/org/top/book_library/db/entity/security/User.java
+++ src/main/java/org/top/book_library/db/entity/security/User.java
@@ -1,20 +1,27 @@
 package org.top.book_library.db.entity.security;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 // таблица пользователей
 @Entity
-@Table(name="user_t")
+@Table(name = "user_t")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Enter your username")
     private String username;                    // уникальный ненулевой логин пользователя
     @Column(nullable = false)
+    @NotBlank(message = "Enter your password")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;                    // пароль пользователя
+
     @ManyToOne
-    @JoinColumn(name="role_id")
+    @JoinColumn(name = "role_id")
     private Role role;                          // роль пользователя
 
     public Long getId() {
