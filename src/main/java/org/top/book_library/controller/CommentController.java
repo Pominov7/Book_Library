85cfa6dcb74b2d4c512dcab2c86c29273de6196f
Pominov7 <Pominov7@mail.ru>
2022-12-18 16:16:57 +0700

    small modifications

--- src/main/java/org/top/book_library/controller/CommentController.java
+++ src/main/java/org/top/book_library/controller/CommentController.java
@@ -3,7 +3,6 @@ package org.top.book_library.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.top.book_library.db.entity.Book;
@@ -11,7 +10,6 @@ import org.top.book_library.db.entity.Comment;
 import org.top.book_library.service.CommentService;
 import org.top.book_library.service.security.UserServiceIml;
 
-import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
